4d176627878a29856cc5e4439d205f59eb2aa698
TRUMANCFY <caifengyutruman@gmail.com>
2020-01-17 21:27:59 +0100

    convert js to flask;db

---
+++ web/backend/server.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, Response, make_response, session
+import requests
+import json
+import argparse
+import logging
+import sys
+
+from flask_cors import CORS
+
+# email
+from flask_mail import Mail
+
+# database import
+from tinydb import TinyDB, where, Query
+db = TinyDB('users.json')
+
+app = Flask(__name__)
+CORS(app)
+
+mail = Mail(app)
+
+
+@app.route('/users/authenticate', methods=['POST'])
+def authenticate():
+    if request.method == 'POST':
+        context = json.loads(request.data)
+
+        # find username first
+        incomingUser = context['username']
+        incomingPwd = context['password']
+
+        if not db.contains(where('username') == incomingUser):
+            return Response(status=404)
+        else:
+            # check the correctness of password
+            dbUser = db.search(where('username') == incomingUser)[0]
+            if dbUser['password'] == incomingPwd:
+                print('found')
+                res = dict()
+                res['id'] = dbUser['id']
+                res['username'] = dbUser['username']
+                res['firstName'] = dbUser['firstName']
+                res['lastName'] = dbUser['lastName']
+                res['token'] = 'random'
+                res = json.dumps(res)
+                return Response(res, status=200)
+            else:
+                return Response(status=404)
+    
+    return Response(status=404)
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    if request.method == 'GET':
+        # return the data of user
+        # user id first name/last name
+        print('Get all users')
+        allUsers = db.all()
+        res = json.dumps(allUsers)
+        return Response(res, status=200)
+        
+    return Response(status=404)
+
+@app.route('/users/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        # check the duplicate
+
+        # if ok, put it into the database
+        # else return failure
+        registerInfo = json.loads(request.data)
+        if db.contains(where('username') == registerInfo['username']):
+            return Response(status=400)
+        else:
+            registerInfo['id'] = len(db)
+            db.insert(registerInfo)
+            return Response(status=200)
+
+    return Response(status=404)
+
+@app.route('/users/<id>', methods=['POST'])
+def getUserInfo(id):
+    if request.method == 'POST':
+        # get information for one user
+        print(id)
+        
+    return Response(status=200)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Config')
+    parser.add_argument("-i", "--ip", type=str, help="Please give the ip", default="127.0.0.1")
+    parser.add_argument("-p", "--port", type=int, help="Please give the running port", default=4000)
+    args = parser.parse_args()
+    app.logger.setLevel(logging.INFO)
+    app.run(debug=True, host=args.ip, port=args.port)
--- web/frontend/src/register/RegisterPage.vue
+++ web/frontend/src/register/RegisterPage.vue
@@ -23,6 +23,11 @@
                 <div v-if="submitted && errors.has('password')" class="invalid-feedback">{{ errors.first('password') }}</div>
             </div>
             <div class="form-group">
+                <label htmlFor="password">Email</label>
+                <input type="password" v-model="user.email" v-validate="{ required: true, min: 6 }" name="email" class="form-control" :class="{ 'is-invalid': submitted && errors.has('email') }" />
+                <div v-if="submitted && errors.has('email')" class="invalid-feedback">{{ errors.first('email') }}</div>
+            </div>
+            <div class="form-group">
                 <button class="btn btn-primary" :disabled="status.registering">Register</button>
                 <img v-show="status.registering" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                 <router-link to="/login" class="btn btn-link">Cancel</router-link>
@@ -41,7 +46,8 @@ export default {
                 firstName: '',
                 lastName: '',
                 username: '',
-                password: ''
+                password: '',
+                email: '',
             },
             submitted: false
         }
