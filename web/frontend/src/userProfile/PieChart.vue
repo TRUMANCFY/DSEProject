936455cef7dee45f4c50bdaf04ae6c481c844a52
TRUMANCFY <caifengyutruman@gmail.com>
2020-01-29 16:46:47 +0100

    add different colors to piechart

--- web/frontend/src/userProfile/PieChart.vue
+++ web/frontend/src/userProfile/PieChart.vue
@@ -4,18 +4,25 @@ import { Pie } from 'vue-chartjs'
 export default {
   extends: Pie,
   props: ['answers', 'data', 'index'],
+  data: function() {
+    return {
+      colors: ['#f87979',  '#41B883','#E46651', '#00D8FF', '#DD1B16'],
+    }
+  },
   mounted () {
     // this.renderChart(this.data, this.options)
 
     console.log(this.answers)
     console.log(this.data)
 
+    var length = this.data.length
+
     this.renderChart({
       labels: this.answers,
       datasets: [
         {
           label: 'GitHub Commits',
-          backgroundColor: '#f87979',
+          backgroundColor: this.colors.slice(0, length),
           data: this.data
         }
       ]
